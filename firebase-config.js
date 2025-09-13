63744f71fdd76d6121348c4e110edbd4cbfefc78
angelsomjgomes-beep <angelsomjgomes@gmail.com>
2025-09-13 14:44:34 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDvfohKa0_vIwdMNZvr4R8t78SnAD86yAA",
+  apiKey: "AIzaSyEXEMPLO123",
   authDomain: "meu-projeto.firebaseapp.com",
   projectId: "meu-projeto",
   storageBucket: "meu-projeto.appspot.com",
   messagingSenderId: "1234567890",
   appId: "1:1234567890:web:abcdef123456"
 };
-
 
