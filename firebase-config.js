cc0f5992336cc5ca8e508c948d573bb9af717910
angelsomjgomes-beep <angelsomjgomes@gmail.com>
2025-09-13 15:13:33 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyCnxE_H-Ys0waD8BiGElmmouwgEyPuMTqA",
-  authDomain: "meu-projeto.firebaseapp.com",
-  projectId: "meu-projeto",
-  storageBucket: "meu-projeto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  authDomain: "achados-e-perdidos-angola.firebaseapp.com",
+  projectId: "achados-e-perdidos-angola",
+  storageBucket: "achados-e-perdidos-angola.firebasestorage.app",
+  messagingSenderId: "388915621666",
+  appId: "1:388915621666:web:805f7ef328a92d52253f7b",
+  measurementId: "G-V1X918H5VK"
 };
-
 
